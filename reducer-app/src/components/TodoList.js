ad543bff656f2bb7b45297317b43f98c6328504a
Jimmique <jimmique88@gmail.com>
2019-11-19 04:15:14 -0800

    Created CRA, Added Reducer folder, Removed default CRA content from App.js, and added a components folder with 3 files

---
+++ reducer-app/src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React, { useState, useReducer } from 'react';
+
+ import { todoReducer, initialTodosState } from '../reducers/todoReducer';
+
+ export default function TodoList() {
+   return (
+     <div>
+     </div>
+   );
+ }
---
+++ reducer-app/src/reducers/todoReducer.js
@@ -0,0 +1,24 @@
+export const initialTodosState = [
+    {
+      item: 'Read',
+      completed: false,
+      id: 1569028265662
+    }, 
+    {
+      item: 'Exercise',
+      completed: false,
+      id: 1569028255640
+    },
+    {
+      item: 'Todos Project',
+      completed: false,
+      id: 1569028365554
+    }
+  ];
+ 
+  export const todoReducer = (state, action) => {
+    switch (action.type) {
+      default: 
+        return state;
+    }
+  };
